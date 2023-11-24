dae05a966273b90f0af8c7ebc52742d7fc4d8154
Martha-K <110817385+Martha-K@users.noreply.github.com>
2023-11-24 21:29:22 +0200

    partially completed homework

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,85 @@
+import Joi from "joi";
+import * as contactsService from "../models/contacts.js";
+import { HttpError } from "../helpers/index.js";
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
+
+const contactUpdateShema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+});
+
+const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({
+      message,
+    });
+  }
+};
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+
+    // if (error) {
+    //   res.status(400).json({ message: `missing required name field` });
+    //   return;
+    // }
+    if (error) {
+        console.log('error', error)
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContacts = async (req, res, next) => {
+  try {
+    const { error } = contactUpdateShema.validate(req.body);
+
+    if (error) {
+        console.log('error', error)
+      throw HttpError(400, error.message);
+    }
+    const {id} = req.params;
+    const result = await contactsService.updateContact(id, req.body)
+    if (!result) {
+        throw HttpError(404, `Not found`);
+      }
+      res.json(result)
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getAllContacts,
+  getById,
+  add,
+  updateContacts,
+};
